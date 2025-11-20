181f1c8ba5e90bc2d541ec378614ecc19ac0cf3b
Dany Luna <danielacrisantoluna@gmail.com>
2025-11-20 14:27:33 -0600

    Agrego frontend/EntertainmentApp como carpeta normal

---
+++ frontend/EntertainmentApp/EntertainmentApp/EntertainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentAppApp.swift
+//  EntertainmentApp
+//
+//  Created by Hulda Daniela Crisanto Luna on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EntertainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/EntertainmentApp/EntertainmentAppTests/EntertainmentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentAppTests.swift
+//  EntertainmentAppTests
+//
+//  Created by Hulda Daniela Crisanto Luna on 20/11/25.
+//
+
+import Testing
+@testable import EntertainmentApp
+
+struct EntertainmentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
